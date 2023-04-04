172ff38aec8040b1c7ebef7d457e0a3348b5aa38
Andrew Loughran <andrew.loughran@digital.cabinet-office.gov.uk>
2023-04-04 16:59:57 +0100

    Removed COMMIT_MESSAGE as needs more work

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -24,10 +24,10 @@ GIT_TAG=$(git describe --tags --first-parent --always)
 
 echo "Writing Lambda provenance"
 yq '.Resources.* | select(has("Type") and .Type == "AWS::Serverless::Function") | .Properties.CodeUri' cf-template.yaml \
-    | xargs -L1 -I{} aws s3 cp "{}" "{}" --metadata "repository=$GITHUB_REPOSITORY,commitsha=$GITHUB_SHA,commitmessage=$COMMIT_MESSAGE,committag=$GIT_TAG"
+    | xargs -L1 -I{} aws s3 cp "{}" "{}" --metadata "repository=$GITHUB_REPOSITORY,commitsha=$GITHUB_SHA,committag=$GIT_TAG"
 echo "Writing Lambda Layer provenance"
 yq '.Resources.* | select(has("Type") and .Type == "AWS::Serverless::LayerVersion") | .Properties.ContentUri' cf-template.yaml \
-    | xargs -L1 -I{} aws s3 cp "{}" "{}" --metadata "repository=$GITHUB_REPOSITORY,commitsha=$GITHUB_SHA,commitmessage=$COMMIT_MESSAGE,committag=$GIT_TAG"
+    | xargs -L1 -I{} aws s3 cp "{}" "{}" --metadata "repository=$GITHUB_REPOSITORY,commitsha=$GITHUB_SHA,committag=$GIT_TAG"
 
 echo "Zipping the CloudFormation template"
 zip template.zip cf-template.yaml
